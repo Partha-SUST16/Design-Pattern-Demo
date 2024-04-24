333ad593aae1f06efb1ed441dd5f8e377fbb8139
Partha-SUST16 <ppartha101@gmail.com>
2024-04-24 14:02:32 +0600

    builder design pattern added

---
+++ src/design_patterns/creational/builder/Main.java
@@ -0,0 +1,11 @@
+package design_patterns.creational.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        //Using builder to get the object in a single line of code and
+        //without any inconsistent state or arguments management issues
+        Computer comp = new Computer.ComputerBuilder(
+                "500 GB", "2 GB").setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true).build();
+    }
+}
